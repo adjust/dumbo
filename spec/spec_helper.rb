614271dd055392870c2cd480336c1f44d8e99a2b
Manuel Kniep <m.kniep@web.de>
2014-07-24 14:36:53 +0200

    allow rspec should syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ RSpec.configure do |config|
   config.order                                            = 'random'
   config.filter_run focus: true
   config.run_all_when_everything_filtered                 = true
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 
   # wrap test in transactions
   config.around(:each) do |example|
